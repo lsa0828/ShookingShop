668aeebb867684b4d0d3195918a4610ed83092fb
lsa0828 <lsa0828@duksung.ac.kr>
2025-04-14 21:32:56 +0900

    feat: add Header

--- shooking-shop/src/App.js
+++ shooking-shop/src/App.js
@@ -1,8 +1,10 @@
+import Header from './Headr';
 import ProductList from './ProduckList';
 
 function App() {
   return (
     <div>
+      <Header />
       <ProductList />
     </div>
   );
---
+++ shooking-shop/src/Headr.js
@@ -0,0 +1,16 @@
+import { TbShoppingBag } from "react-icons/tb";
+
+function Header() {
+  return (
+    <header className="h-16 bg-black flex items-center px-16">
+      <div className="relative w-fit ml-auto">
+        <TbShoppingBag className="text-3xl text-white" />
+        <div className="absolute -bottom-1 -right-2 bg-white text-black text-xs font-semibold w-5 h-5 rounded-full flex items-center justify-center">
+          3
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
