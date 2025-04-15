6c57343f6d1cd3d90e92111c202b3f2f73735c36
lsa0828 <lsa0828@duksung.ac.kr>
2025-04-15 17:28:13 +0900

    test: add ProductCard.test.js to check rendering

--- shooking-shop/src/ProduckList.js
+++ shooking-shop/src/ProduckList.js
@@ -4,11 +4,11 @@ function ProductList() {
   const cards = [];
   const cardContents = [
     {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
-    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
-    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
-    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
-    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
-    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드B", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드C", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드D", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드E", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드F", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
   ]
   for(let i=0; i<cardContents.length; i++) {
     cards.push(<ProductCard key={i} content={cardContents[i]} />);
---
+++ shooking-shop/src/ProductCard.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import ProductCard from "./ProductCard";
+
+test("renders ProductCard", () => {
+  const content = {
+    image: "temp_shoes.jpeg",
+    brand: "브랜드A",
+    description: "편안하고 착용감이 좋은 신발",
+    price: "35,000원"
+  };
+
+  render(<ProductCard content={content}/>);
+
+  expect(screen.getByText("브랜드A")).toBeInTheDocument();
+  expect(screen.getByRole("img")).toHaveAttribute("src", "temp_shoes.jpeg");
+  expect(screen.getByText("편안하고 착용감이 좋은 신발")).toBeInTheDocument();
+  expect(screen.getByText("35,000원")).toBeInTheDocument();
+});
