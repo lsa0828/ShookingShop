ced28952176c556c0b5990fbb21b139a13fa46f5
lsa0828 <lsa0828@duksung.ac.kr>
2025-04-15 16:39:21 +0900

    fix: modify ProductCard to use props

--- shooking-shop/src/ProduckList.js
+++ shooking-shop/src/ProduckList.js
@@ -2,11 +2,19 @@ import ProductCard from "./ProductCard";
 
 function ProductList() {
   const cards = [];
-  for(let i=0; i<6; i++) {
-    cards.push(<ProductCard key={i} />);
+  const cardContents = [
+    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+    {image: "temp_shoes.jpeg", brand: "브랜드A", description: "편안하고 착용감이 좋은 신발", price: "35,000원"},
+  ]
+  for(let i=0; i<cardContents.length; i++) {
+    cards.push(<ProductCard key={i} content={cardContents[i]} />);
   }
   return (
-    <div className="w-full flex justify-center p-6">
+    <div className="flex justify-center p-6">
       <div className="grid grid-cols-2 gap-4">
         {cards}
       </div>
