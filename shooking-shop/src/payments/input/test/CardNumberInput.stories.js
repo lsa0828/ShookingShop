1a8d754ebcd0b5d015b1359a0a222fb40a9c948e
lsa0828 <lsa0828@duksung.ac.kr>
2025-04-29 01:54:39 +0900

    test: update CardNumberInput

--- shooking-shop/src/payments/input/test/CardNumberInput.stories.js
+++ shooking-shop/src/payments/input/test/CardNumberInput.stories.js
@@ -1,33 +1,25 @@
 import { useEffect, useState } from "react";
 import CardNumberInput from "../CardNumberInput";
-import { expect, userEvent, within } from "@storybook/test";
 
 export default {
   title: 'Payments/input/CardNumberInput',
-  component: CardNumberInput
+  component: CardNumberInput,
+  args: {
+    cardNumber: '',
+  },
+  render: (args) => {
+    const [cardNumber, setCardNumber] = useState(args.cardNumber);
+    useEffect(() => {
+      setCardNumber(args.cardNumber);
+    }, [args.cardNumber]);
+    return (
+      <CardNumberInput cardNumber={cardNumber} setCardNumber={setCardNumber} />
+    );
+  }
 };
 
-const Template = ({cardNumber, ...args}) => {
-  const [cardNumberTest, setCardNumberTest] = useState(cardNumber);
-  useEffect(() => {
-    setCardNumberTest(cardNumber);
-  }, [cardNumber]);
-  return (
-    <CardNumberInput cardNumber={cardNumberTest} setCardNumber={setCardNumberTest} {...args} />
-  );
-}
-
-export const Default = Template.bind({});
-Default.args = {
-  cardNumber: '',
+export const Default = {
+  args: {
+    cardNumber: "1234567894561231"
+  }
 };
-Default.play = async ({canvasElement}) => {
-  const canvas = within(canvasElement);
-  const input = await canvas.getByRole('textbox');
-
-  // 카드 번호 입력력
-  await userEvent.type(input, '1234567894560123');
-
-  // 결과가 마스킹된 값으로 나오는지 확인
-  expect(input.value).toBe('1234-5678-●●●●-●●●●');
-}
