ffa38c50684a67d23be8ec91b4f388680a072f81
lsa0828 <lsa0828@duksung.ac.kr>
2025-05-13 13:14:24 +0900

    test: add stories to cart

---
+++ shooking-shop/src/cart/test/InCartList.stories.js
@@ -0,0 +1,18 @@
+import InCartList from "../InCartList";
+
+export default {
+  title: 'Cart/InCartList',
+  component: InCartList,
+  args: {
+  },
+  render: () => {
+    return (
+      <InCartList />
+    );
+  }
+};
+
+export const Default = {
+  args: {
+  }
+};
---
+++ shooking-shop/src/cart/test/PayInCartButton.stories.js
@@ -0,0 +1,18 @@
+import PayInCartButton from "../PayInCartButton";
+
+export default {
+  title: 'Cart/PayInCartButton',
+  component: PayInCartButton,
+  args: {
+  },
+  render: () => {
+    return (
+      <PayInCartButton />
+    );
+  }
+};
+
+export const Default = {
+  args: {
+  }
+};
